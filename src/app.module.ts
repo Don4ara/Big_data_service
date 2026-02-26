3f9dcc3411ef799c3758d3be38490f964c22eb2e
don4ara <don4araa@gmail.com>
2026-02-26 21:29:42 +0500

    chore: добавлен файл для примера токена апи и объявление конфига во всем модуле приложения

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataVitrineModule } from './data-vitrine/data-vitrine.module';
 
 @Module({
-  imports: [DataVitrineModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DataVitrineModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
