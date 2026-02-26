5efcb0129dd235487d9a75f41e15d2d69628f748
don4ara <don4araa@gmail.com>
2026-02-26 15:54:36 +0500

    feat: добавлен импорт для работы генерации синтетических данных

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { DataVitrineModule } from './data-vitrine/data-vitrine.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [DataVitrineModule],
   controllers: [AppController],
   providers: [AppService],
 })
