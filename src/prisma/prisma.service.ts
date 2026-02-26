aa910981ab7b1af0116c9974589dbc39d5916e45
don4ara <don4araa@gmail.com>
2026-02-26 15:51:54 +0500

    feat: добавление модулей для последующей подключение БД

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,7 +3,12 @@ import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
-    }
+  constructor() {
+    // @ts-ignore
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
