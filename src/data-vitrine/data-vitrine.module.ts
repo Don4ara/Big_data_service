82f08f6e1d7fec4ca71eced6028113552774634f
don4ara <don4araa@gmail.com>
2026-04-01 19:15:51 +0500

    refactor: разбиение логики на разные файлы

--- src/data-vitrine/data-vitrine.module.ts
+++ src/data-vitrine/data-vitrine.module.ts
@@ -3,11 +3,19 @@ import { HttpModule } from '@nestjs/axios';
 import { DataVitrineController } from './data-vitrine.controller';
 import { DataVitrineService } from './data-vitrine.service';
 import { GeocodingService } from './geocoding.service';
+import { OrderDbService } from './services/order-db.service';
+import { OrderGeneratorService } from './services/order-generator.service';
 import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [DataVitrineController],
-  providers: [DataVitrineService, GeocodingService, PrismaService],
+  providers: [
+    DataVitrineService,
+    GeocodingService,
+    PrismaService,
+    OrderDbService,
+    OrderGeneratorService,
+  ],
 })
-export class DataVitrineModule { }
+export class DataVitrineModule {}
